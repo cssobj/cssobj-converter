26fa6e5fe3081bd3cf381ffa92704cd335932a3b
1111hui <26634873@qq.com>
2016-07-02 22:30:42 +0800

    ADD: CORS for http server; error check and debug

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 var convert = require('../src/cssobj-converter.js')
 var http = require('http')
+var debug = require('debug')('cssobj')
 
 var port = 8080
-var server = http.createServer((req,res)=>{
+var server = http.createServer((req, res) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
   res.writeHead(200, {'Content-Type': 'text/plain'})
   if (req.method == 'POST') {
     var bodyString = ''
@@ -11,14 +18,15 @@ var server = http.createServer((req,res)=>{
     })
 
     req.on('end', function () {
-      try{
+      try {
         var json = JSON.parse(bodyString || '')
-        console.log(json)
+        debug(bodyString)
         var result = convert(json.text)
-      } catch(e){
+      } catch(e) {
+        debug(e)
         return res.end('')
       }
-      res.end( JSON.stringify(result) )
+      res.end(JSON.stringify(result, null, 2))
     })
   }
-}).listen(port, e=>console.log('Listen on ', port))
+}).listen(port, e => console.log('Listen on ', port))
